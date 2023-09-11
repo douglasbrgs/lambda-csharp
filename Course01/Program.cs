ba12c356880bc2c7e6ae3645a91624f7c9fc11da
Douglas Borges <douglasborges19@gmail.com>
2023-09-11 20:26:01 -0300

    Initial commit

---
+++ Course01/Program.cs
@@ -0,0 +1,25 @@
+﻿using Course01.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Course01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("TV", 900.0));
+            products.Add(new Product("Notebook", 1200.0));
+            products.Add(new Product("Tablet", 450.0));
+
+            products.Sort();
+
+            foreach (Product prod in products)
+            {
+                Console.WriteLine(prod);
+            }
+        }
+    }
+}
