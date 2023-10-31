037a942e4eac790840776fb29789c3658486d917
Douglas Borges <douglasborges19@gmail.com>
2023-10-31 19:00:51 -0300

    expressão lambda inline

--- Course01/Program.cs
+++ Course01/Program.cs
@@ -14,9 +14,7 @@ namespace Course01
             products.Add(new Product("Notebook", 1200.0));
             products.Add(new Product("Tablet", 450.0));
 
-            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-            products.Sort(comp);
+            products.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
             foreach (Product prod in products)
             {
